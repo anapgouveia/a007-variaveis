beddd47d55afbb2926d1dd6342e51c9d6edc427e
anapgouveia <anapgouveia97@gmail.com>
2022-07-19 18:52:23 -0300

    tarefa feita

---
+++ exercicio1/script.js
@@ -0,0 +1,18 @@
+//let nome = 
+//let idade =
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+
+//a variavel foi impressa undefined pois nao tem valor atribuido a ela
+
+
+const nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+
+//antes a variavel para nome estava let e quando mudei para const o erro sumiu
+
+console.log (nome)
+console.log (idade)
+
+console.log ("Olá", nome, "você tem", idade, "anos" )
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let a = 30
+let b = 40
+let c
+
+c = a
+a = b
+b = c 
+
+console.log("A", a)
+console.log("B", b)
